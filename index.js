67f090bb59b18584205c715b8d2bf391624c14ff
sachin2503 <115262637+sachin2503@users.noreply.github.com>
2023-09-05 08:13:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,195 @@
+
+const wrapper = document.querySelector(".wrapper1");
+const wrapper6 = document.querySelector(".wrapper6");
+
+const Btn = document.querySelector(".Btn");
+const userImg = document.querySelector("[data-image]");
+const userImg1 = document.querySelector("[data-image1]");
+const userName = document.querySelector("[data-userName]");
+const userName1 = document.querySelector("[data-userName1]");
+const userPRN = document.querySelector("[data-userPRN]");
+const userPRN1 = document.querySelector("[data-userPRN1]");
+const userYear = document.querySelector("[data-userYear]");
+const userYear1 = document.querySelector("[data-userYear1]");
+const userExamYearMonth = document.querySelector("[data-ExamYearMonth]");
+const userExamYearMonth1 = document.querySelector("[data-ExamYearMonth1]");
+const mother = document.querySelector("[data-userMotherName]");
+const mother1 = document.querySelector("[data-userMotherName1]");
+const sem = document.querySelector("[data-sem]");
+const sem1 = document.querySelector("[data-sem1]");
+const resultDec = document.querySelector("[data-resultDeclaration]");
+const resultDec1 = document.querySelector("[data-resultDeclaration1]");
+const status2 = document.querySelector("[data-resultStatus]");
+const status1 = document.querySelector("[data-resultStatus1]"); 
+const rej = document.querySelector("[data-SGPA-creditRegistered]");
+const rej1 = document.querySelector("[data-SGPA-creditRegistered1]");
+const rej3 = document.querySelector("[data-CGPA-creditRegistered]");
+const rej4 = document.querySelector("[data-CGPA-creditRegistered1]");
+const ear1 = document.querySelector("[data-SGPA-creditEarned]");
+const ear2 = document.querySelector("[data-SGPA-creditEarned1]");
+const ear3 = document.querySelector("[data-CGPA-creditEarned]");
+const ear4 = document.querySelector("[data-CGPA-creditEarned1]");
+const SGPA = document.querySelector("[data-SGPA]");
+const SGPA1 = document.querySelector("[data-SGPA1]");
+const CGPA = document.querySelector("[data-CGPA]");
+const CGPA1 = document.querySelector("[data-CGPA1]");
+const code1 = document.querySelector("[subCode-1]");
+const code1_1 = document.querySelector("[subCode-1-1]");
+const code2 = document.querySelector("[subCode-2]");
+const code2_1 = document.querySelector("[subCode-2-1]");
+const code3 = document.querySelector("[subCode-3]");
+const code3_1 = document.querySelector("[subCode-3-1]");
+const code4 = document.querySelector("[subCode-4]");
+const code4_1 = document.querySelector("[subCode-4-1]");
+const code5 = document.querySelector("[subCode-5]");
+const code5_1 = document.querySelector("[subCode-5-1]");
+const code6 = document.querySelector("[subCode-6]");
+const code6_1 = document.querySelector("[subCode-6-1]");
+const code7 = document.querySelector("[subCode-7]");
+const code7_1 = document.querySelector("[subCode-7-1]");
+const code8 = document.querySelector("[subCode-8]");
+const code8_1 = document.querySelector("[subCode-8-1]");
+const code9 = document.querySelector("[subCode-9]");
+const code9_1 = document.querySelector("[subCode-9-1]");
+const name1 = document.querySelector("[subName-1]");
+const name1_1 = document.querySelector("[subName-1-1]");
+const name2 = document.querySelector("[subName-2]");
+const name2_1 = document.querySelector("[subName-2-1]");
+const name3 = document.querySelector("[subName-3]");
+const name3_1 = document.querySelector("[subName-3-1]");
+const name4 = document.querySelector("[subName-4]");
+const name4_1 = document.querySelector("[subName-4-1]");
+const name5 = document.querySelector("[subName-5]");
+const name5_1 = document.querySelector("[subName-5-1]");
+const name6 = document.querySelector("[subName-6]");
+const name6_1 = document.querySelector("[subName-6-1]");
+const name7 = document.querySelector("[subName-7]");
+const name7_1 = document.querySelector("[subName-7-1]");
+const name8 = document.querySelector("[subName-8]");
+const name8_1 = document.querySelector("[subName-8-1]");
+const name9 = document.querySelector("[subName-9]");
+const name9_1 = document.querySelector("[subName-9-1]");
+const credit1 = document.querySelector("[subCredit-1]");
+const credit1_1 = document.querySelector("[subCredit-1-1]");
+const credit2 = document.querySelector("[subCredit-2]");
+const credit2_1 = document.querySelector("[subCredit-2-1]");
+const credit3 = document.querySelector("[subCredit-3]");
+const credit3_1 = document.querySelector("[subCredit-3-1]");
+const credit4 = document.querySelector("[subCredit-4]");
+const credit4_1 = document.querySelector("[subCredit-4-1]");
+const credit5 = document.querySelector("[subCredit-5]");
+const credit5_1 = document.querySelector("[subCredit-5-1]");
+const credit6 = document.querySelector("[subCredit-6]");
+const credit6_1 = document.querySelector("[subCredit-6-1]");
+const credit7 = document.querySelector("[subCredit-7]");
+const credit7_1 = document.querySelector("[subCredit-7-1]");
+const credit8 = document.querySelector("[subCredit-8]");
+const credit8_1 = document.querySelector("[subCredit-8-1]");
+const credit9 = document.querySelector("[subCredit-9]");
+const credit9_1 = document.querySelector("[subCredit-9-1]");
+const grade1 = document.querySelector("[subGrade-1]");
+const grade1_1 = document.querySelector("[subGrade-1-1]");
+const grade2 = document.querySelector("[subGrade-2]");
+const grade2_1 = document.querySelector("[subGrade-2-1]");
+const grade3 = document.querySelector("[subGrade-3]");
+const grade3_1 = document.querySelector("[subGrade-3-1]");
+const grade4 = document.querySelector("[subGrade-4]");
+const grade4_1 = document.querySelector("[subGrade-4-1]");
+const grade5 = document.querySelector("[subGrade-5]");
+const grade5_1 = document.querySelector("[subGrade-5-1]");
+const grade6 = document.querySelector("[subGrade-6]");
+const grade6_1 = document.querySelector("[subGrade-6-1]");
+const grade7 = document.querySelector("[subGrade-7]");
+const grade7_1 = document.querySelector("[subGrade-7-1]");
+const grade8 = document.querySelector("[subGrade-8]");
+const grade8_1 = document.querySelector("[subGrade-8-1]");
+const grade9 = document.querySelector("[subGrade-9]");
+const grade9_1 = document.querySelector("[subGrade-9-1]");
+const date = document.querySelector("[data-Date]");
+const date1 = document.querySelector("[data-Date1]");
+
+
+
+          // if(date1.value !=="" && grade9_1.value !=="" && grade8_1.value !=="" && grade7_1.value !=="" && grade6_1.value !=="" && grade5_1.value !=="" && grade4_1.value !=="" && grade3_1.value !=="" &&
+          // grade2_1.value !=="" && grade1_1.value !=="" && credit9_1.value !=="" && credit8_1.value !=="" && credit7_1.value !=="" && credit6_1.value !=="" && credit5_1.value !=="" && credit4_1.value !=="" && 
+          // credit3_1.value !=="" && credit2_1.value !=="" && credit1_1.value !=="" && name9_1.value !=="" && name8_1.value !=="" && name7_1.value !=="" && name6_1.value !=="" && name5_1.value !=="" &&
+          // name4_1.value !=="" && name3_1.value !=="" && name2_1.value !=="" && name1_1.value !=="" && code1_1.value !=="" && code2_1.value !=="" && code3_1.value !=="" && code4_1.value !=="" && code5_1.value !=="" &&
+          // code6_1.value !=="" && code7_1.value !=="" && code8_1.value !=="" && code9_1.value !=="" && CGPA1.value !=="" && SGPA1.value !=="" && ear2.value !=="" &&
+          // ear4.value !=="" && rej1.value !=="" && rej4.value !=="" && status1.value !=="" && resultDec1.value !=="" && sem1.value !=="" && mother1.value !=="" &&
+          // userExamYearMonth1.value !=="" && userYear1.value !=="" && userPRN1.value !=="" && userName1.value !=="" ){
+          //     Btn.classList.add("active");
+          // }
+          // if(status1.innerText !== " "){
+          //   Btn.classList.add("active");
+          // }
+         
+
+
+Btn.addEventListener('click' , () =>
+{
+    wrapper.classList.add("active");
+    wrapper6.classList.add("active");
+
+    userImg.src = URL.createObjectURL(userImg1.files[0]);
+    console.log( URL.createObjectURL(userImg1.files[0]));
+    console.log(userName1.value);
+    userName.innerText = userName1.value;
+    userPRN.innerText = userPRN1.value;
+    userYear.innerText = userYear1.value;
+    userExamYearMonth.innerText = userExamYearMonth1.value
+    mother.innerText = mother1.value;
+    sem.innerText = sem1.value;
+    resultDec.innerText = resultDec1.value;
+    status2.innerText = status1.value;
+    rej.innerText = rej1.value;
+    rej3.innerText = rej4.value;
+    ear1.innerText = ear2.value;
+    ear3.innerText = ear4.value;
+    SGPA.innerText = SGPA1.value;
+    CGPA.innerText = CGPA1.value;
+    console.log(code1_1.value);
+    code1.innerText = code1_1.value;
+    code2.innerText = code2_1.value;
+    code3.innerText = code3_1.value;
+    code4.innerText = code4_1.value;
+    code5.innerText = code5_1.value;
+    code6.innerText = code6_1.value;
+    code7.innerText = code7_1.value;
+    code8.innerText = code8_1.value;
+    code9.innerText = code9_1.value;
+    name1.innerText = name1_1.value;
+    name2.innerText = name2_1.value;
+    name3.innerText = name3_1.value;
+    name4.innerText = name4_1.value;
+    name5.innerText = name5_1.value;
+    name6.innerText = name6_1.value;
+    name7.innerText = name7_1.value;
+    name8.innerText = name8_1.value;
+    name9.innerHTML = name9_1.value;
+    credit1.innerText = credit1_1.value;
+    credit2.innerText = credit2_1.value;
+    credit3.innerText = credit3_1.value;
+    credit4.innerText = credit4_1.value;
+    credit5.innerText = credit5_1.value;
+    credit6.innerText = credit6_1.value;
+    credit7.innerText = credit7_1.value;
+    credit8.innerText = credit8_1.value;
+    credit9.innerText = credit9_1.value;
+    grade1.innerText = grade1_1.value;
+    grade2.innerText = grade2_1.value;
+    grade3.innerText = grade3_1.value;
+    grade4.innerText = grade4_1.value;
+    grade5.innerText = grade5_1.value;
+    grade6.innerText = grade6_1.value;
+    grade7.innerText = grade7_1.value;
+    grade8.innerText = grade8_1.value;
+    grade9.innerText = grade9_1.value;
+    date.innerText = date1.value;
+    
+    
+
+
+    
+})
+
+
